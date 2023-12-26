fe03308e7af1a64d0addcf78bcf7bbc57b8d5f90
Duong Nam <dnam.dvn@gmail.com>
2023-12-26 16:52:17 +0700

    update - get data from database -> train model LSTM

--- dags/stock_dag.py
+++ dags/stock_dag.py
@@ -1,27 +1,22 @@
 from datetime import datetime, timedelta, date
-import time
-import sys
+import numpy as np
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-import numpy as np
-import ssl
+from sqlalchemy import create_engine
 
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 from vnstock import *
-def crawl_data():
-    current_date = datetime.now().date().strftime('%Y-%m-%d')
-    stock_data = stock_historical_data(symbol="TCH", start_date="2020-01-01", end_date=current_date, resolution="1D", type="stock", beautify=True, decor=False, source='DNSE')
-    if not stock_data.empty:
-        csv_file_name = "/opt/airflow/plugins/stock_data.csv"
-        stock_data.to_csv(csv_file_name, index=False)
-        print(f"Dữ liệu đã được lưu vào tệp {csv_file_name}")
-    else:
-        print("Không có dữ liệu để lưu.")
-    return True
+def get_data():
+    engine = create_engine('postgresql://airflow:airflow@172.18.0.3:5432/stock_db')
+    table_name = 'd1'
+    query = f'SELECT * FROM {table_name};'
+    df = pd.read_sql(query, engine)
+    return df
 def create_dataset(dataset, time_step=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-time_step-1):
@@ -30,7 +25,7 @@ def create_dataset(dataset, time_step=1):
 		dataY.append(dataset[i + time_step, 0])
 	return np.array(dataX), np.array(dataY)
 def train_model():
-    df = pd.read_csv("/opt/airflow/plugins/stock_data.csv")
+    df = get_data()
     df1=df.reset_index()['close']
     scaler=MinMaxScaler(feature_range=(0,1))
     df1=scaler.fit_transform(np.array(df1).reshape(-1,1))
@@ -41,7 +36,7 @@ def train_model():
     # reshape into X=t,t+1,t+2,t+3 and Y=t+4
     time_step = 100
     X_train, y_train = create_dataset(train_data, time_step)
-    X_test, ytest = create_dataset(test_data, time_step)
+    X_test, y_test = create_dataset(test_data, time_step)
     
     # reshape input to be [samples, time steps, features] which is required for LSTM
     X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
@@ -53,8 +48,8 @@ def train_model():
     model.add(LSTM(50))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error',optimizer='adam')
-    model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=10,batch_size=64,verbose=1)
-    model.save("/opt/airflow/plugins/stockmodel.h5")
+    callback_model = tf.keras.callbacks.ModelCheckpoint('/opt/airflow/plugins/stockmodel.h5', monitor='val_loss')
+    history = model.fit(x = X_train, y = y_train, validation_data=(X_test, y_test), epochs = 10, batch_size = 64, callbacks = [callback_model])
     return True
 
 default_args = {
@@ -68,23 +63,18 @@ default_args = {
 }
 
 with DAG(
-        dag_id='stock_dag',
+        dag_id='train_model_stock_dag',
         default_args=default_args,
         description='Train model stock',
         start_date=datetime(2023, 12, 23),
         schedule_interval='@daily',
         tags=['stock_data']
 ) as dag:
-    
-    crawl_data = PythonOperator(
-        task_id='crawl_data',
-        python_callable=crawl_data,
-        dag=dag
-    )
+
     train_model = PythonOperator(
-        task_id='train_model',
+        task_id='train_stock_model',
         python_callable=train_model,
         dag=dag
     )
-    crawl_data >> train_model
+    train_model
         
